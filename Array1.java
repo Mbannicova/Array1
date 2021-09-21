1ef7a747f5d07da230ca501a3e891de78b2f8839
Mbannicova <87451655+Mbannicova@users.noreply.github.com>
2021-09-21 13:22:01 +0300

    Update Array1.java

--- Array1.java
+++ Array1.java
@@ -1,41 +1,45 @@
-
 public class Array1 {
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
+  Integer[] data = { 100, 200, 300, 400, null };
 
-		Integer[] data = { 100, 200, 300, 400, null };
-		shiftToTheRight(data);
-		for( int i : data) {
-			System.out.println(i);
-		}
-		
-		System.out.println();
-		
-		//shiftToTheLeft(data);
-		for( int i : data) {
-		System.out.println(i);
-		}
-		
-		
-	}
+  shiftToTheRight(data);
+  for( Object i : data) {
+   System.out.println(i);
+  }
 
+        System.out.println("------------------");
+  
+  shiftToTheLeft(data);
+  for( Object i : data) {
+      System.out.println(i);
+  }  
+ }
+	
+// 1. Создается метод с массивом, принимающим значения массива типа integer/
+// 2. Создается цикл, в котором переменной i присваивается длина массива - 1, при этом i не должно быть равно нулю.  
+// 3. i шагает в начало массива и заменяет значение ячейки ранее стоящей на значение ячейки впереди стоящей.
+// 4. Последней ячейке с индексом [0] присваивается значение null;
+// 5. Измененный массив возвращается в main.
+	
+	
+ public static Integer[] shiftToTheRight(Integer[] array){
+     for ( int i = array.length - 1; i > 0; i-- ){
+         array[i] = array[i - 1];
+     }
+     array[0] = null;
+     return array;
+ }
 
-	   public static Integer[] shiftToTheRight(Integer[] array){
-	        int i = array.length - 1 ;
-	        while( i > 0 ){
-	            array[i--] = array[i];
-	        }
-	        array[i] = null;
-	        return array;
-	    }
-	   
-	    //Integer[] data = { 100, 200, 300, 400, null };	
-	    public static Integer[] shiftToTheLeft(Integer[] array){
-	        int i = 0;
-	        while( i < array.length -1 ){
-	            array[i++] = array[i];
-	        }
-	       array[i] = null;
-	        return array;
-	    }
+    public static Integer[] shiftToTheLeft (Integer[] array){
+        for ( int i = 1; i < array.length; i++ ){
+            array[i -1] = array[i];
+        }
+        array[array.length - 1] = null;
+        return array;
+    }
+    
+   
 }
+//Примитивные целые числа Java не могут быть null , но класс Integer ,
+//который обертывает примитив int , может быть  с null.
